8c8ba3276f0807388dd3cc100e53d5007015d62a
Nhu Nguyen Tran Binh <binhnhu1409@gmail.com>
2023-01-20 05:24:00 +0200

    assignment 1 done

--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
 ﻿using System;
+using System.Text;
 using System.Globalization;
 
 namespace CSharpFundamental
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            /* Challenge 1 */
-            string text = "this is a text";
-            string newText = toTitleCase(text);
-            Console.WriteLine(newText); // expect to see "This Is A Text"
+      /* Challenge 1 */
+      string text = "this is a text";
+      string newText = toTitleCase(text);
+      Console.WriteLine(newText); // expect to see "This Is A Text"
 
-            /* Challenge 2 */
-            int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
-            int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
-            int[,] result = matrixMultiply(arrayA, arrayB);
-            Console.WriteLine(result); //share your findings to Slack
-        }
-        static string toTitleCase(string input)
-        {
-            /* Write your code here */
-        }
-        static int[,] matrixMultiply(int[,] array1, int[,] array2)
-        {
-            /* Write your code here */
-        }
+      /* Challenge 2 */
+      int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
+      int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
+      // int[,] result = matrixMultiply(arrayA, arrayB);
+      // Console.WriteLine(result); //share your findings to Slack
     }
+    static string toTitleCase(string input)
+    {
+      /* Write your code here */
+      string[] splitedText = input.Split(' ');
+      string upperText = "";
+      foreach (string word in splitedText)
+      {
+        int firstChar = (byte)word[0];
+        if (firstChar >= 97 && firstChar <= 122)
+        {
+          firstChar = firstChar - 32;
+        }
+        upperText += (char)firstChar + word[1..word.Length] + ' ';
+      }
+      return upperText;
+    }
+    // static int[,] matrixMultiply(int[,] array1, int[,] array2)
+    // {
+    //     /* Write your code here */
+    // }
+  }
 }
